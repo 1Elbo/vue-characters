f8391f2092dc194f4054dcf704a9d4b3cdf2a7c8
Petr Bačík <pebaweby@gmail.com>
2024-07-13 11:46:01 +0200

    created CHaracterInput

--- src/components/CharacterInput.vue
+++ src/components/CharacterInput.vue
@@ -1,7 +1,8 @@
 <template>
-    <div>
-
-    </div>
+    <form @submit.prevent="addNewCharacter">
+    <input type="text" required="true" placeholder="Add character here" v-model="newChar"/><br>
+    <button class="button-30" role="button" >Add Character</button>
+  </form>
 </template>
 
 <script>
