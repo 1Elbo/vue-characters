f21505e68bbdead3ee840890903dfb546e6c82dd
Petr Bačík <pebaweby@gmail.com>
2024-07-13 12:57:16 +0200

    zprovozněno characterRemove

--- src/components/Character.vue
+++ src/components/Character.vue
@@ -1,7 +1,7 @@
 <template>
     
     {{ name }}
-      <div class="cross" @click="removeCharacter">
+      <div class="cross" @click="returnIndex">
         <div class="bar1"></div>
         <div class="bar2"></div>
       </div>
@@ -14,6 +14,17 @@
             name: {
                 type: String
             },
+            id:{
+                type: Number
+            }
+        },
+        methods: {
+            returnIndex() {
+               /* this.$emit("return-index", id )  */
+               this.$emit("return-index", this.id)
+               
+               
+            }
         },
     }
 </script>
