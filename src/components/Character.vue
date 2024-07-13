b04eae9912221e48534bf83c31c74e4fc56bc300
Petr Bačík <pebaweby@gmail.com>
2024-07-13 11:34:41 +0200

    zprovozněn komponent Character

--- src/components/Character.vue
+++ src/components/Character.vue
@@ -1,14 +1,20 @@
 <template>
-    {{ char }}
+    
+    {{ name }}
       <div class="cross" @click="removeCharacter">
         <div class="bar1"></div>
         <div class="bar2"></div>
       </div>
+    
 </template>
 
 <script>
     export default {
-        
+        props: {
+            name: {
+                type: String
+            },
+        },
     }
 </script>
 
